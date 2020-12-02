237c130a68d2020ee018248492f059155abae8fb
Dion Low <dionlow17@gmail.com>
2020-12-02 08:49:05 -1000

    Removes code from separate project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React from "react"
-
-import useForm from "./useForm"
-import Inputs from "./components/Inputs"
-
-import {
-  container,
-  formContainer,
-  circleIcon,
-  iconImage,
-  headerText,
-  signupButton,
-} from "./styles.module.css"
+import { container, formContainer, headerText } from "./styles.module.css"
 
 const App = () => {
-  const { formState, onChangeFormValue, onSubmit } = useForm()
-  const inputProps = { formState, onChangeFormValue }
-
   return (
     <div className={container}>
       <div className={formContainer}>
-        <div className={circleIcon}>
-          <span className={iconImage} />
-        </div>
-        <div className={headerText}>Tell us about yourself</div>
-        <Inputs {...inputProps} />
-        <button
-          type="button"
-          className={signupButton}
-          onClick={onSubmit}
-          onKeyPress={onSubmit}
-        >
-          Sign-up
-        </button>
+        <h1 className={headerText}>Starter project</h1>
       </div>
     </div>
   )
